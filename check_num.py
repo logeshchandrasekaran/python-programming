940889f8d0c8595737bd5a2509a15bcbba0ebf53
logeshchandrasekaran <37336302+logeshchandrasekaran@users.noreply.github.com>
2018-04-09 17:52:44 +0530

    Create check_num.py

---
+++ check_num.py
@@ -0,0 +1,7 @@
+num=int(input("enter the number"))
+if(num>1 and num<100000):
+  print("the number is positive")
+if(num<0):
+  print("the number is negative")
+if(num=0):
+  print("the number is zero")
