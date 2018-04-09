e80d025d1e6f2d4534192c5b7f9313d07db993cb
logeshchandrasekaran <37336302+logeshchandrasekaran@users.noreply.github.com>
2018-04-09 18:13:04 +0530

    Create numb_oddeven.py

---
+++ numb_oddeven.py
@@ -0,0 +1,9 @@
+num=int(input("enter the number"))
+if(num>1 and num<100000):
+  if(num%2==1):
+    print("the number is odd")
+  else:
+    print("the number is even")
+else:
+  print("the number is not in range of 1-100000")
+  
