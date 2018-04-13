ffb3c53716b0ce95387181a473145666c2c813e9
logeshchandrasekaran <37336302+logeshchandrasekaran@users.noreply.github.com>
2018-04-13 18:56:45 +0530

    Create function_as_argument.py

---
+++ beginner level/function_as_argument.py
@@ -0,0 +1,8 @@
+def exploder(x,n):
+     return x * n 
+def fun(fun1,aa, stri):
+     a=fun1(stri,aa)
+     print(a)
+stri=input("enter the string")
+num=int(input("enter the number of times"))
+my_fun(exploder,num,stri)
