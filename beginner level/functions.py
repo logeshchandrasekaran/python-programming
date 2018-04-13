158249d5662c16be18737e615341b2202ec09ebe
logeshchandrasekaran <37336302+logeshchandrasekaran@users.noreply.github.com>
2018-04-13 17:27:23 +0530

    Create functions.py

---
+++ beginner level/functions.py
@@ -0,0 +1,7 @@
+def oddeven(temp):
+    if temp%2==1:
+        print("the given number is odd")
+    else:
+        print("the given number is even")
+num=int(input("enter the number"))
+oddeven(num)
